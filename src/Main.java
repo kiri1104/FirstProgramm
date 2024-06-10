4c7eebe2efcda2df2a600a25247532f952372bd8
kiri1104 <kirillgoroshko2004@gmail.com>
2024-06-10 17:01:25 +0300

    лаб2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 import javax.swing.*;
 
 public class Main {
+
     public static void main(String[] args) {
+        System.out.println("Hello world!");
 
-        System.out.println("Hello, World!");
-
-        JFrame f = new JFrame( "");
+        JFrame f = new JFrame("Swing Paint Demo");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         MyPanel myPanel = new MyPanel();
         myPanel.repaint();
+        myPanel.setSymbols("0,1,2,3,4,5,6,7,8,9,Д,О,М");
         f.add(myPanel);
         f.pack();
         f.setVisible(true);
